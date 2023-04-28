7aed705faaeb5ca483b382c546344d521032d886
githubuiux <uiuxista@gmail.com>
2023-04-28 10:45:25 +0300

    Initial Commit

---
+++ EnvironmentProperty/EnvironmentPropertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentPropertyApp.swift
+//  EnvironmentProperty
+//
+//  Created by Levent on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentPropertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
